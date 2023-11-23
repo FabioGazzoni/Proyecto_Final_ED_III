595a45220397a06d9e587a803d801027722a9227
Fabio Gazzoni <fabio.gazzoni@mi.unc.edu.ar>
2023-11-22 23:08:01 -0300

    Done tp

--- src/Proyecto_Final.c
+++ src/Proyecto_Final.c
@@ -9,27 +9,14 @@
 #include "lpc17xx_gpdma.h"
 
 #define TIME_TIMER 49999999
-#define DMA_SIZE 1
 
 void configGPIO();
 void configTimer0();
 void configADC();
 void configUART();
 
-void configPin();
-void configDAC();
-void configDMA();
-uint32_t getProportionalValue(uint32_t input);
-//void pwmGenerator(uint8_t duty_cycle);
-//uint8_t getDutyCycle(uint32_t value);
-
-// void configTimer1();
-// void TIMER1_IRQHandler();
-
 uint16_t info = 0b00;
-uint32_t output_dac[2] = {0,(1023<<6)};
 uint8_t status_bomb_uart = 0;
-GPDMA_Channel_CFG_Type GPDMACfg;
 
 int main(void)
 {
@@ -39,20 +26,8 @@ int main(void)
 	configTimer0();
 	configADC();
 
-//	configPin();
-//	configDAC();
-//	configDMA();
-
 	while (1)
 	{
-//				for (int var = 0; var < 10000000; ++var) {
-//				}
-//				ADC_StartCmd(LPC_ADC, ADC_START_NOW);
-//				if (output_dac[1] == 0) {
-//								output_dac[1] = (1023 <<6);
-//							} else {
-//								output_dac[1] = 0;
-//							}
 	}
 	return 0;
 }
@@ -86,7 +61,6 @@ void ADC_IRQHandler()
 {
 	info = ((LPC_ADC->ADDR0) >> 4) & 0xFFF;
 
-//	output_dac[1] = (getProportionalValue(info)<<6);
 
 	UART_Send(LPC_UART2, &info, sizeof(info), BLOCKING);
 
@@ -181,106 +155,4 @@ void UART2_IRQHandler(void){
 				}
         }
         return;
-}
-
-
-void configPin()
-{
-	PINSEL_CFG_Type pinCfg;
-	pinCfg.Funcnum = PINSEL_FUNC_2;
-	pinCfg.OpenDrain = 0;
-	pinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
-	pinCfg.Portnum = 0;
-	pinCfg.Pinnum = 26;
-	PINSEL_ConfigPin(&pinCfg);
-}
-
-void configDAC()
-{
-	DAC_CONVERTER_CFG_Type dacCfg;
-	dacCfg.CNT_ENA = SET;
-	dacCfg.DMA_ENA = SET;
-	DAC_Init(LPC_DAC);
-	/*
-	x = (PCLK_DAC [25] * BIAS_Frec [1x10⁶])/(Frec_salida[1] * N°_Muestr [1])
-	x = 25000000
-	 */
-
-	/*Set timeout*/
-	uint32_t tmp;
-	tmp = 10000; // cada cuantos ticks pide el dato a la DMA;
-	DAC_SetDMATimeOut(LPC_DAC, tmp);
-	DAC_ConfigDAConverterControl(LPC_DAC, &dacCfg);
-}
-
-void configDMA()
-{
-	GPDMA_LLI_Type LLI1;
-	LLI1.SrcAddr = (uint32_t)output_dac;
-	LLI1.DstAddr = (uint32_t)&(LPC_DAC->DACR);
-	LLI1.NextLLI = (uint32_t)&LLI1;
-	LLI1.Control = 	2 			//Cant datos
-					| (2 << 18) // source width 32 bits
-					| (2 << 21) | (1<<26) ;// dest width 32 bits
-
-	GPDMA_Init();
-
-	GPDMACfg.ChannelNum = 7;
-	GPDMACfg.SrcMemAddr = (uint32_t)(output_dac);
-	GPDMACfg.DstMemAddr = 0;
-	GPDMACfg.TransferSize = 2;
-	GPDMACfg.TransferWidth = 0;
-	GPDMACfg.TransferType = GPDMA_TRANSFERTYPE_M2P;
-	GPDMACfg.SrcConn = 0;
-	GPDMACfg.DstConn = GPDMA_CONN_DAC;
-	GPDMACfg.DMALLI = (uint32_t)&LLI1;
-	GPDMA_Setup(&GPDMACfg);
-
-	GPDMA_ChannelCmd(7, ENABLE);
-}
-
-//void pwmGenerator(uint8_t duty_cycle)
-//{
-//	for (int i = 0; i < DMA_SIZE; i++)
-//	{
-//		if (i < duty_cycle)
-//		{
-//			output_dac[i] = (0xffffffff << 6);
-//		}
-//		else
-//		{
-//			output_dac[i] = 0;
-//		}
-//	}
-//}
-//
-//uint8_t getDutyCycle(uint32_t value)
-//{
-//	uint32_t min = 1700;
-//	uint32_t max = 3300;
-//
-//	if(value < min){
-//		return 0;
-//	}else if(value > max){
-//		return 100;
-//	}else {
-//		return (value - min)/((max - min)/100);
-//	}
-//
-//}
-
-uint32_t getProportionalValue(uint32_t input){
-	uint32_t min = 1700;
-	uint32_t max = 3300;
-
-	if(input < min){
-			return 0;
-		}else if(input > max){
-			return 1023;
-		}
-	uint32_t num = (input - min)*1023;
-	uint32_t den = (max - min);
-	uint32_t val = num/den;
-	return val;
-
 }
